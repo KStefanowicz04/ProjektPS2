05b775c52b5730e9bfe5b6a6b5286c975f84889b
KStefanowicz04 <kacper.stefanowicz04@gmail.com>
2025-04-08 15:07:46 +0200

    Dodano kod

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        double[] tablica = new double[1000000];
+        for(int i=0; i<1000000; i++) {
+            tablica[i] = Math.random();
+            System.out.println("Wartosc na indeksie " + i + ": " + tablica[i]);
+        }
+
     }
 }
